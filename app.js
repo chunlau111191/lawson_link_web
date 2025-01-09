f2420f26a2607920211f92cfad96b297e4b544c0
chunlau111191 <72881205+chunlau111191@users.noreply.github.com>
2025-01-09 21:54:28 +0800

    1

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+
+
+// set up a route to respond to GET requests to the root URL　main.html
+app.use(express.static("public"));
+
+
+// Set up the server to listen on port 8080
+const port = 8080;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}
+);
+
